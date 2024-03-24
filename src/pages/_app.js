55259b46ef4a21b662e23cc463c724dc2841cceb
Jesse <jessealexanderthompson@gmail.com>
2024-03-24 01:42:27 -0400

    Remove elements doc

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,29 +3,12 @@
 import React from 'react';
 import '../styles/globals.scss'; // Place style import declarations in /styles/styles.scss and actual style css files under /styles/appstyles/
 import '../styles/features/tailwind.css';
-import Head from 'next/head';
 import { Internal } from '/modules/internal/';
-import {
-  GoogleGsiClient,
-  GoogleSignInRegister,
-} from '/modules/internal/localImports';
 import { ThemeProvider } from '../components/provider';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className='antialiased'>
-      <Head>
-        <meta
-          name='google-signin-client_id'
-          content='169701902623-9a74mqcbqr38uj87qm8tm3190cicaa7m.apps.googleusercontent.com'
-        />
-        <title>{pageProps.siteTitle}</title>
-      </Head>
-      <>
-        {GoogleGsiClient}
-        {GoogleSignInRegister}
-      </>
-
       <ThemeProvider attribute='class' defaultTheme='dark'>
         <Internal {...pageProps} _MasterPageComponent={Component} />
       </ThemeProvider>
