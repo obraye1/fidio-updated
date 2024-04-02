f531d5ebef4334b574e8485a3190a8aa9775c8e4
bryan <obraye05@gmail.com>
2024-04-02 21:59:00 +0100

    inital commit

--- modules/article/templates.js
+++ modules/article/templates.js
@@ -1,1 +1,18 @@
-const basic={container:{margin:"0 1rem"},internalContainer:{margin:"0 auto",maxWidth:"800px"}};export default{basic:basic};
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var basic = {
+  container: {
+    margin: '0 1rem'
+  },
+  internalContainer: {
+    margin: '0 auto',
+    maxWidth: '800px'
+  }
+};
+var _default = exports["default"] = {
+  basic: basic
+};
